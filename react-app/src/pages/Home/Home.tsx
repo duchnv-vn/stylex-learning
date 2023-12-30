7450c38ed15192c9703efae9926f222d6a28dec4
duchnv-vn <huynhnguyenvietduc@gmail.com>
2023-12-30 23:53:23 +0700

    Init source code & install stylex

---
+++ react-app/src/pages/Home/Home.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as stylex from '@stylexjs/stylex';
+
+export default function Home() {
+  const styles = stylex.create({});
+
+  return <section>Home page</section>;
+}
